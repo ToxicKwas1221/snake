200f862f6e3fb31a593b573a852849613a62655a
ToxicKwas1221 <57598894+ToxicKwas1221@users.noreply.github.com>
2019-12-04 13:21:54 +0100

    Update snake.py

--- snake.py
+++ snake.py
@@ -11,77 +11,63 @@ BACKGROUD_COLOR = (53, 53, 53)
 APPLE_COLOR = (255, 0, 0)
 
 
-score = 0
-taken_spaces = []
-end_of_tail = None
 pygame.init()
-screen = pygame.display.set_mode((500, 500))
+screen = pygame.display.setmode((500, 500))
+pygame.display.set_caption(CAPTION)
 class Game:
     def __init__(self):
-        pygame.display.set_caption(CAPTION)
-        screen.fill((BACKGROUD_COLOR))
-        pygame.display.update()
         self.snake = Snake()
         self.apple = Apple()
+
     def start(self):
         while True:
             for event in pygame.event.get():
                 if event.type == QUIT:
                     pygame.quit()
                     sys.exit()
-                if event.type == KEYDOWN and event.key == K_RIGHT:
-                    self.snake.direction = 0
-                if event.type == KEYDOWN and event.key == K_UP:
-                    self.snake.direction = 1
                 if event.type == KEYDOWN and event.key == K_DOWN:
                     self.snake.direction = 2
-                if event.type == KEYDOWN and event.key == K_LEFT:
-                    self.snake.direction = 3
+                    self.snake.move()
+    def food_eaten(self):
+        if self.snake.head == self.apple.position:
+            return True
+        else:
+            return False
 
 
 class Snake:
     def __init__(self):
-        self.direction = 0
-        self.x = 40
-        self.y = 40
-        self.length = 3
+        self.body = [self.tail, {"x" : 50, "y" : 30}, {"x" : 60, "y" : 30}]  # coords of body parts
+        self.head = {"x" : 70, "y" : 30}
+        self.tail = {"x" : 40, "y" : 30}
+        self.direction = None
 
     def move(self):
         if self.direction == 0:  # Right
-            pygame.draw.rect(screen, SNAKE_COLOR, pygame.Rect(self.x+10, self.y+10, 10, 10))
-            pygame.display.flip()
+            self.head["x"] += 10
+            for num, part in enumerate(self.body):  # FIXME
+                self.body[num] = self.body[num+1]
+            pygame.display.update()
         if self.direction == 1:  # Up
-            pygame.draw.rect(screen, SNAKE_COLOR, pygame.Rect(self.x, self.y-10, 10, 10))
-            pygame.display.flip()
+
         if self.direction == 2:  # Down
-            pygame.draw.rect(screen, SNAKE_COLOR, pygame.Rect(self.x, self.y+10, 10, 10))
-            pygame.display.flip()
+
         if self.direction == 3:  # Left
-            pygame.draw.rect(screen, SNAKE_COLOR, pygame.Rect(self.x-10, self.y, 10, 10))
-            pygame.display.flip()
 
 
 class Apple:
-    def __init__(self):
+    def __init__(self):  # generates position
         while True:
             self.x = random.randrange(0, 510, 10)
             self.y = random.randrange(0, 510, 10)
-            self.position = (self.x, self.y)
-            if self.position in taken_spaces:
+            self.position = {"x" : self.x, "y" : self.y}
+            if self.position in game.snake.body or self.position == game.snake.head:
                 continue
             else:
                 break
 
-    def spawn(self):
-        pygame.draw.rect(screen, APPLE_COLOR, pygame.Rect(self.x, self.y, 10, 10))
-        pygame.display.flip()
+    def spawn(self):  #
+        pass
 
-
-
-
-
-
-
-
-
-# THE END
+game = Game()
+game.start()
